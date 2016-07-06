80f9ced95f632a7b13af0859ab8d9063cb82aff6
Adrian Witas <awitas@specificmedia.com>
2016-07-06 12:55:33 -0700

    Removed license from go file, leave it as separate license file

--- config.go
+++ config.go
@@ -1,23 +1,3 @@
-/*
- *
- *
- * Copyright 2012-2016 Viant.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- *  use this file except in compliance with the License. You may obtain a copy of
- *  the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- *  License for the specific language governing permissions and limitations under
- *  the License.
- *
- */
-
-// Package dsc - Datastore config
 package dsc
 
 import (
@@ -28,12 +8,12 @@ import (
 
 //Config represent datastore config.
 type Config struct {
-	DriverName        string
-	PoolSize          int
-	MaxPoolSize       int
-	Descriptor        string
-	EncodedParameters string
-	Parameters        map[string]string
+	DriverName          string
+	PoolSize            int
+	MaxPoolSize         int
+	Descriptor          string
+	Parameters          map[string]string
+	SecretParametersURL string //url to JSON object, this delegates credential or secret out of dev
 }
 
 //Get returns value for passed in parameter name or panic - please use Config.Has to check if value is present.
@@ -64,22 +44,18 @@ func (c *Config) Has(name string) bool {
 
 //Init makes parameter map from encoded parameters if presents, expands descriptor with parameter value using [param_name] matching pattern.
 func (c *Config) Init() {
-	if len(c.EncodedParameters) > 0 && len(c.Parameters) == 0 {
-		c.Parameters = toolbox.MakeStringMap(c.EncodedParameters, ":", ",")
-	}
-	descriptor := c.Descriptor
 	for key := range c.Parameters {
 		macro := "[" + key + "]"
-		descriptor = strings.Replace(descriptor, macro, c.Parameters[key], 1)
+		c.Descriptor = strings.Replace(c.Descriptor, macro, c.Parameters[key], 1)
 	}
-	c.Descriptor = descriptor
 }
 
 //NewConfig creates new Config, it takes the following parameters
 // descriptor - optional datastore connection string with macros that will be looked epxanded from for instance [user]:[password]@[url]
 // encodedParameters should be in the following format:   <key1>:<value1>, ...,<keyN>:<valueN>
 func NewConfig(driverName string, descriptor string, encodedParameters string) *Config {
-	result := &Config{DriverName: driverName, PoolSize: 1, MaxPoolSize: 2, Descriptor: descriptor, EncodedParameters: encodedParameters, Parameters: make(map[string]string)}
+	var parameters = toolbox.MakeStringMap(encodedParameters, ":", ",")
+	result := &Config{DriverName: driverName, PoolSize: 1, MaxPoolSize: 2, Descriptor: descriptor, Parameters:parameters}
 	result.Init()
 	return result
 }
