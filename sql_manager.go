6fae617c7b067b07e1de7e5a7da3572619988a05
adrianwit <adrianwit@gmail.com>
2018-02-25 21:43:03 -0800

    added sql/query logging option

--- sql_manager.go
+++ sql_manager.go
@@ -6,6 +6,10 @@ import (
 	"reflect"
 )
 
+var Logf = func(format string, args ... interface{}) {
+
+}
+
 func asSQLDb(wrapped interface{}) (*sql.DB, error) {
 	if result, ok := wrapped.(*sql.DB); ok {
 		return result, nil
@@ -41,11 +45,27 @@ type sqlManager struct {
 	*AbstractManager
 }
 
+func (m *sqlManager) initConnectionIfNeeded(connection Connection) error {
+	if sqlConnection, ok := connection.(*sqlConnection); ok {
+		if sqlConnection.init {
+			return nil
+		}
+		sqlConnection.init = true
+		dialect := GetDatastoreDialect(m.config.DriverName)
+		return dialect.Init(m, connection)
+	}
+	return nil
+}
+
 func (m *sqlManager) ExecuteOnConnection(connection Connection, sql string, args []interface{}) (sql.Result, error) {
 	db, err := asSQLDb(connection.Unwrap(sqlDbPointer))
+	if err == nil {
+		err = m.initConnectionIfNeeded(connection)
+	}
 	if err != nil {
 		return nil, err
 	}
+
 	var executable sqlExecutor = db
 	tx, err := asSQLTx(connection.Unwrap(sqlTxtPointer))
 	if err != nil {
@@ -58,9 +78,20 @@ func (m *sqlManager) ExecuteOnConnection(connection Connection, sql string, args
 		args = make([]interface{}, 0)
 	}
 
-	//fmt.Printf("SQL: %v\n", sql)
+	if len(args) > 0 {
+		dialect := GetDatastoreDialect(m.config.DriverName)
+		sql = dialect.NormalizePlaceholders(sql)
+	}
+
+	if sqlConnection, ok := connection.(*sqlConnection); ok {
+		if ! sqlConnection.init {
+
+		}
+	}
 
 	result, err := executable.Exec(sql, args...)
+
+	Logf("[%v]:%v %v", m.config.username, sql, args)
 	if err != nil {
 		return nil, fmt.Errorf(fmt.Sprintf("failed to execute %v %v on %v due to:\n%v", sql, args, m.Manager.Config().Parameters, err.Error()))
 	}
@@ -69,9 +100,19 @@ func (m *sqlManager) ExecuteOnConnection(connection Connection, sql string, args
 
 func (m *sqlManager) ReadAllOnWithHandlerOnConnection(connection Connection, query string, args []interface{}, readingHandler func(scanner Scanner) (toContinue bool, err error)) error {
 	db, err := asSQLDb(connection.Unwrap((*sql.DB)(nil)))
+	if err == nil {
+		err = m.initConnectionIfNeeded(connection)
+	}
 	if err != nil {
 		return err
 	}
+
+	if len(args) > 0 {
+		dialect := GetDatastoreDialect(m.config.DriverName)
+		query = dialect.NormalizePlaceholders(query)
+	}
+
+	Logf("[%v]:%v", m.config.username, query)
 	sqlStatement, sqlError := db.Prepare(query)
 	if sqlError != nil {
 		return fmt.Errorf("failed to prepare sql: %v with %v due to:%v\n\t", query, args, sqlError.Error())
